ab91c5ecc26a82148f34b525015d0f6748f08e2b
LuizLobo-TM <luizeduardosilvalobo@outlook.com>
2021-04-14 16:30:26 -0300

    first commit

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What king of creature is Tahm Kench?",
+    answer: "Spirit",
+    options: [
+      "Darkin",
+      "Yordle",
+      "Human (cursed)",
+      "Spirit"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Does Dr. Mundo have a doctorate?",
+    answer: "No",
+    options: [
+      "No",
+      "Yes",
+      "Yes, but it is a filme degree",
+      "Mundo say greed is good!"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many possible kinds of minions are there on Summoners Rift? (Colors do not matter)",
+    answer: "4",
+    options: [
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the gromp's full name? ",
+    answer: "Lord Grompulus Kevin Ribbiton of Croaksworth",
+    options: [
+      "Grompule",
+      "Lord Grompulus Kevin Ribbiton of Croaksworth",
+      "Gromp",
+      "Sir Grompleton"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the name of Gragas' ultimate ability?",
+    answer: "Explosive Cask",
+    options: [
+      "Happy Hour",
+      "Explosive Cask",
+      "Drunken Rage",
+      "Barrel Roll"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
